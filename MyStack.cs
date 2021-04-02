14c5df7f077a75ce23cfc23f95fa86f1696f93c0
redars <41670606+redars@users.noreply.github.com>
2021-04-02 15:08:17 +0300

    Add files via upload

---
+++ MyStack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SAOD_1
+{
+    class MyStack <T>
+    {
+        private T[] data;
+        private int size;
+        private int Capacity;
+        public MyStack()
+        {
+            size = 0;
+            Capacity = 1;
+            data = new T[0];
+        }
+        public T Peek()
+        {
+            if(size!=0)
+            {
+                return data[size - 1];
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+        public void Push(T x)
+        {
+            if (size == Capacity - 1)
+            {
+                Capacity *= 2;
+                Array.Resize(ref data, Capacity);
+            }
+            size++;
+            data[size - 1] = x;
+        }
+        public void Pop()
+        {
+            if (size!=0)
+            {
+                Array.Resize(ref data, --size);
+            }
+        }
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+        public void Clear()
+        {
+            size = 0;
+            data = new T[0];
+        }
+    }
+}
---
+++ VectorList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectorList
+{
+    class VectorList<T>
+    {
+        int size;
+        T [] data;
+        public VectorList()
+        {
+            data = new T[0];
+        }
+        public void Add(T t)
+        {
+            data[size - 1] = t;
+        }
+        public void Insert(T t,int i)
+        {
+            data[i] = t;
+        }
+        public void RemoveAt(int i)
+        {
+            data[i].
+        }
+    }
+}
